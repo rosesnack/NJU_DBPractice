ddc39838541a6880ca22f9d76d2900c7d656a528
Chen Dedan <221220159@smail.nju.edu.cn>
2024-11-30 15:28:03 +0800

    update

--- src/storage/buffer/buffer_pool_manager.cpp
+++ src/storage/buffer/buffer_pool_manager.cpp
@@ -42,21 +42,117 @@ BufferPoolManager::BufferPoolManager(DiskManager *disk_manager, wsdb::LogManager
   }
 }
 
-auto BufferPoolManager::FetchPage(file_id_t fid, page_id_t pid) -> Page * { WSDB_STUDENT_TODO(l1, t2); }
+auto BufferPoolManager::FetchPage(file_id_t fid, page_id_t pid) -> Page * { 
+  std::lock_guard<std::mutex> lock(latch_);
+  auto it = page_frame_lookup_.find({fid, pid});
+  if (it != page_frame_lookup_.end()) { //the page is in the frame
+    frame_id_t frame_id = it->second;
+    frames_[frame_id].Pin();
+    replacer_->Pin(frame_id);
+    return frames_[frame_id].GetPage();
+  }
+  else {
+    frame_id_t frame_id = GetAvailableFrame();
+    UpdateFrame(frame_id, fid, pid);
+    return frames_[frame_id].GetPage();
+  }
+}
 
-auto BufferPoolManager::UnpinPage(file_id_t fid, page_id_t pid, bool is_dirty) -> bool { WSDB_STUDENT_TODO(l1, t2); }
+auto BufferPoolManager::UnpinPage(file_id_t fid, page_id_t pid, bool is_dirty) -> bool {
+  std::lock_guard<std::mutex> lock(latch_);
+  auto it = page_frame_lookup_.find({fid, pid});
+  if (it == page_frame_lookup_.end()) return false;
+  frame_id_t frame_id = it->second;
+  auto &frame = frames_[frame_id];
+  if (!frame.InUse()) return false;
 
-auto BufferPoolManager::DeletePage(file_id_t fid, page_id_t pid) -> bool { WSDB_STUDENT_TODO(l1, t2); }
+  frame.Unpin();
+  if (!frame.InUse()) replacer_->Unpin(frame_id);
+  if (is_dirty) frame.SetDirty(true);
+  return true;
+}
 
-auto BufferPoolManager::DeleteAllPages(file_id_t fid) -> bool { WSDB_STUDENT_TODO(l1, t2); }
+auto BufferPoolManager::DeletePage(file_id_t fid, page_id_t pid) -> bool { 
+  std::lock_guard<std::mutex> lock(latch_);
+  auto it = page_frame_lookup_.find({fid, pid});
+  if (it == page_frame_lookup_.end()) return true;
+  frame_id_t frame_id = it->second;
+  auto &frame = frames_[frame_id];
+  if (frame.InUse()) return false;
 
-auto BufferPoolManager::FlushPage(file_id_t fid, page_id_t pid) -> bool { WSDB_STUDENT_TODO(l1, t2); }
+  if (frame.IsDirty()) 
+    disk_manager_->WritePage(fid, pid, frame.GetPage()->GetData());
+  frame.Reset();
+  page_frame_lookup_.erase(it);
+  free_list_.push_back(frame_id);
+  replacer_->Unpin(frame_id);
+  return true;
+}
 
-auto BufferPoolManager::FlushAllPages(file_id_t fid) -> bool { WSDB_STUDENT_TODO(l1, t2); }
+auto BufferPoolManager::DeleteAllPages(file_id_t fid) -> bool { 
+  bool suc = true;
+  for (auto it = page_frame_lookup_.begin(); it != page_frame_lookup_.end(); ++it)
+    if (it->first.fid == fid)
+      if (!DeletePage(fid, it->first.pid))
+        suc = false;
+  return suc;
+}
 
-auto BufferPoolManager::GetAvailableFrame() -> frame_id_t { WSDB_STUDENT_TODO(l1, t2); }
+auto BufferPoolManager::FlushPage(file_id_t fid, page_id_t pid) -> bool {
+  std::lock_guard<std::mutex> lock(latch_);
+  auto it = page_frame_lookup_.find({fid, pid});
+  if (it == page_frame_lookup_.end()) return false;
+  frame_id_t frame_id = it->second;
+  auto &frame = frames_[frame_id];
 
-void BufferPoolManager::UpdateFrame(frame_id_t frame_id, file_id_t fid, page_id_t pid) { WSDB_STUDENT_TODO(l1, t2); }
+  if (frame.IsDirty()) {
+    disk_manager_->WritePage(fid, pid, frame.GetPage()->GetData());
+    frame.SetDirty(false);
+  }
+  return true;
+}
+
+auto BufferPoolManager::FlushAllPages(file_id_t fid) -> bool {
+  bool suc = true;
+  for (auto it = page_frame_lookup_.begin(); it != page_frame_lookup_.end(); ++it)
+    if (it->first.fid == fid)
+      if (!FlushPage(fid, it->first.pid))
+        suc = false;
+  return suc;
+}
+
+auto BufferPoolManager::GetAvailableFrame() -> frame_id_t {
+  if (!free_list_.empty()) {
+    frame_id_t frame_id = free_list_.front();
+    free_list_.pop_front();
+    return frame_id;
+  }
+  frame_id_t frame_id;
+  replacer_->Victim(&frame_id);
+  if (frame_id == -1) {
+    WSDB_THROW(WSDB_NO_FREE_FRAME, "buffer pool manager cannot find available frame to load page");
+  }
+  return frame_id;
+}
+
+void BufferPoolManager::UpdateFrame(frame_id_t frame_id, file_id_t fid, page_id_t pid) {
+  auto &frame = frames_[frame_id];
+  file_id_t old_fid = frame.GetPage()->GetFileId();
+  page_id_t old_pid = frame.GetPage()->GetPageId();
+  if (frame.IsDirty())
+    disk_manager_->WritePage(old_fid, old_pid, frame.GetPage()->GetData());
+
+  auto it = page_frame_lookup_.find({old_fid, old_pid});
+  if (it != page_frame_lookup_.end())
+    page_frame_lookup_.erase(it);
+
+  frame.Reset();
+  disk_manager_->ReadPage(fid, pid, frame.GetPage()->GetData());
+  frame.GetPage()->SetFilePageId(fid, pid);
+  frames_[frame_id].Pin();
+  replacer_->Pin(frame_id);
+  page_frame_lookup_[{fid, pid}] = frame_id;
+}
 
 auto BufferPoolManager::GetFrame(file_id_t fid, page_id_t pid) -> Frame *
 {
--- src/storage/buffer/replacer/lru_replacer.cpp
+++ src/storage/buffer/replacer/lru_replacer.cpp
@@ -26,12 +26,55 @@ namespace wsdb {
 
 LRUReplacer::LRUReplacer() : cur_size_(0), max_size_(BUFFER_POOL_SIZE) {}
 
-auto LRUReplacer::Victim(frame_id_t *frame_id) -> bool { WSDB_STUDENT_TODO(l1, t1); }
+auto LRUReplacer::Victim(frame_id_t *frame_id) -> bool  { 
+    //WSDB_STUDENT_TODO(l1, t1); 
+    std::lock_guard<std::mutex> lock(latch_);
+    //使用反向迭代器从后往前遍历
+    for (auto rit = lru_list_.rbegin(); rit != lru_list_.rend(); ++rit) {
+        if (rit->second == true) { 
+            frame_id_t fid = rit->first;
+            *frame_id = fid;
+            lru_list_.erase((++rit).base());  //rit.base()指向的是it视角下rit指向元素的下一个
+            lru_hash_.erase(fid);
+            --cur_size_; 
+            return true;
+        }
+    }
+    return false;
+ }
 
-void LRUReplacer::Pin(frame_id_t frame_id) { WSDB_STUDENT_TODO(l1, t1); }
+void LRUReplacer::Pin(frame_id_t frame_id) {
+    //WSDB_STUDENT_TODO(l1, t1); 
+    std::lock_guard<std::mutex> lock(latch_); 
+    auto it = lru_hash_.find(frame_id);
+    if (it != lru_hash_.end()) {
+        if (it->second->second == true) { 
+            it->second->second = false;
+            --cur_size_;
+        }
+        lru_list_.splice(lru_list_.begin(), lru_list_, it->second);  
+    }
+    else
+        lru_list_.push_front({frame_id, false});
+    lru_hash_[frame_id] = lru_list_.begin();
+ }
 
-void LRUReplacer::Unpin(frame_id_t frame_id) { WSDB_STUDENT_TODO(l1, t1); }
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+    //WSDB_STUDENT_TODO(l1, t1);  
+    std::lock_guard<std::mutex> lock(latch_);
+    auto it = lru_hash_.find(frame_id);
+    if (it != lru_hash_.end()) {
+        if (it->second->second == false) {
+            it->second->second = true;
+            ++cur_size_;
+        }
+    }
+ }
 
-auto LRUReplacer::Size() -> size_t { WSDB_STUDENT_TODO(l1, t1); }
+auto LRUReplacer::Size() -> size_t {
+    //WSDB_STUDENT_TODO(l1, t1); 
+    std::lock_guard<std::mutex> lock(latch_);
+    return cur_size_;
+}
 
 }  // namespace wsdb
